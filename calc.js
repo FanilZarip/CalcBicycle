037be23a823c4c892e8461e910c8adfd3364f1e1
FanilZarip <fanilfan1994@mail.ru>
2022-02-23 16:35:40 +0300

    with moduleJS

--- calc.js
+++ calc.js
@@ -1,14 +1,4 @@
-// console.dir(document.body);
-
-let result = document.getElementById('result');
-let equal = document.getElementById('equal');
-let sum = document.getElementById('sum');
-let div = document.getElementById('div');
-let mult = document.getElementById('mult');
-let sub = document.getElementById('sub');
-let full__clear = document.querySelector('.full__clear');
-let backSpace = document.querySelector('.clear__backspace');
-let numbers = document.querySelectorAll('.number__button'); // Get all buttons with numbers. It will make object. 
+import {result, equal, sum, div, mult, sub, full__clear, backSpace, numbers} from './view.js';
 
 let mathS = {
     sum,
@@ -19,114 +9,109 @@ let mathS = {
 
 
 let resultsForNum = '';
-
 let mathOperation = null;
 let correctedResult = null;                   
-
-
 let num1 = '';
 let num2 = '';
+
 
 console.log(numbers); //Print created objects to console log
 
 
-    resultsForNum = 0;
+resultsForNum = 0;
+
+for (const number of numbers) {
+
+    number.addEventListener('click', printNum1);
+
+    function printNum1 () {
+
+        resultsForNum = resultsForNum + number.innerText;
+
+        let resultsForNumLength = resultsForNum.length;
+
+        if (resultsForNumLength > 6) {
+            result.style.fontSize = "45px"
+        } else {
+            result.style.fontSize = "96px"
+        }
+        result.innerText = +resultsForNum;
+        
+        num1 = +resultsForNum;
+
+    }
 
     
 
-    for (const number of numbers) {
-
+    function printNum2 () {
+        resultsForNum = resultsForNum + number.innerText;
+        result.innerText = +resultsForNum;
+        num2 = +resultsForNum;
         
+        let resultsForNumLength = resultsForNum.length;
 
+        if (resultsForNumLength > 6) {
+            result.style.fontSize = "45px"
+        }
+        
+    }
+    
+    for (const key in mathS) {
+        function goToGetNum2 () {
+            mathOperation = mathS[key].id
+            number.removeEventListener('click', printNum1);
+            number.addEventListener('click', printNum2);
+
+            backSpace.removeEventListener('click', deleteSymbolNum1);
+            backSpace.addEventListener('click', deleteSymbolNum2);
+
+            resultsForNum = 0;
+        console.log('ReallyNum1', num1);
+        console.log('ReallyNum2', num2);
+        }
+        mathS[key].addEventListener('click', goToGetNum2);
+    }  
+
+
+    equal.addEventListener('click', startCalculating);
+    full__clear.addEventListener('click', resetAll);
+
+    function resetAll () {
+        number.removeEventListener('click', printNum2);
         number.addEventListener('click', printNum1);
+        backSpace.addEventListener('click', deleteSymbolNum1);
+        backSpace.removeEventListener('click', deleteSymbolNum2);
+
+        resultsForNum = 0;
+        num1 = 0;
+        num2 = 0;
+        result.innerText = +resultsForNum;
+        result.style.fontSize = "96px";
+    }
+
+    function startCalculating () {
+        backSpace.addEventListener('click', deleteSymbolNum1);
+        backSpace.removeEventListener('click', deleteSymbolNum2);
+        number.removeEventListener('click', printNum2);
+        Calc();
+        number.addEventListener('click', printNum1);
+        resultsForNum = 0;
+    }   
     
-        function printNum1 () {
+}
 
-            resultsForNum = resultsForNum + number.innerText;
+backSpace.addEventListener('click', deleteSymbolNum1);
+function deleteSymbolNum1 () {
+    resultsForNum = resultsForNum.substr(0, resultsForNum.length - 1);
+    result.innerText = +resultsForNum;
+    num1 = resultsForNum;
+}
 
-            let resultsForNumLength = resultsForNum.length;
-
-            if (resultsForNumLength > 6) {
-                result.style.fontSize = "45px"
-            } else {
-                result.style.fontSize = "96px"
-            }
-            result.innerText = +resultsForNum;
-            num1 = +resultsForNum;
-
-        }
-
-        
-
-        function printNum2 () {
-            resultsForNum = resultsForNum + number.innerText;
-            result.innerText = +resultsForNum;
-            num2 = +resultsForNum;
-            
-            let resultsForNumLength = resultsForNum.length;
-
-            if (resultsForNumLength > 6) {
-                result.style.fontSize = "45px"
-            }
-            
-        }
-        
-        for (const key in mathS) {
-            function goToGetNum2 () {
-                mathOperation = mathS[key].id
-                number.removeEventListener('click', printNum1);
-                number.addEventListener('click', printNum2);
-
-                backSpace.removeEventListener('click', deleteSymbolNum1);
-                backSpace.addEventListener('click', deleteSymbolNum2);
-
-                resultsForNum = 0;
-            console.log('ReallyNum1', num1);
-            console.log('ReallyNum2', num2);
-            }
-            mathS[key].addEventListener('click', goToGetNum2);
-        }  
-
-
-        equal.addEventListener('click', startCalculating);
-        full__clear.addEventListener('click', resetAll);
-
-        function resetAll () {
-            number.removeEventListener('click', printNum2);
-            number.addEventListener('click', printNum1);
-            backSpace.addEventListener('click', deleteSymbolNum1);
-            backSpace.removeEventListener('click', deleteSymbolNum2);
-
-            resultsForNum = 0;
-            num1 = 0;
-            num2 = 0;
-            result.innerText = +resultsForNum;
-            result.style.fontSize = "96px";
-        }
-
-        function startCalculating () {
-            backSpace.addEventListener('click', deleteSymbolNum1);
-            backSpace.removeEventListener('click', deleteSymbolNum2);
-            number.removeEventListener('click', printNum2);
-            Calc();
-            number.addEventListener('click', printNum1);
-            resultsForNum = 0;
-        }   
-        
-    }
-
-    backSpace.addEventListener('click', deleteSymbolNum1);
-    function deleteSymbolNum1 () {
-        resultsForNum = resultsForNum.substr(0, resultsForNum.length - 1);
-        result.innerText = +resultsForNum;
-        num1 = resultsForNum;
-    }
-
-    function deleteSymbolNum2 () {
-        resultsForNum = resultsForNum.substr(0, resultsForNum.length - 1);
-        result.innerText = +resultsForNum;
-        num2 = resultsForNum;
-    }
+function deleteSymbolNum2 () {
+    resultsForNum = resultsForNum.substr(0, resultsForNum.length - 1);
+    result.innerText = +resultsForNum;
+    num2 = resultsForNum;
+}
 
 function Calc () {
 
---
+++ view.js
@@ -0,0 +1,9 @@
+export const result = document.getElementById('result');
+export const equal = document.getElementById('equal');
+export const sum = document.getElementById('sum');
+export const div = document.getElementById('div');
+export const mult = document.getElementById('mult');
+export const sub = document.getElementById('sub');
+export const full__clear = document.querySelector('.full__clear');
+export const backSpace = document.querySelector('.clear__backspace');
+export const numbers = document.querySelectorAll('.number__button'); // Get all buttons with numbers. It will make object. 
